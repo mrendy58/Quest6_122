9f8f9c51672a842bfcd0c97cd4bc715e7dde5a62
mrendy58 <derendyyy58@gmail.com>
2024-11-22 13:42:52 +0700

    membuat fungsi objeck untuk memilih matakuliah

---
+++ app/src/main/java/com/example/p6meeting8/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.p6meeting8.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
+}
