5cf3def15a6d3c3e718eb361189ab8ccd9664a69
mrendy58 <derendyyy58@gmail.com>
2024-11-22 13:19:17 +0700

    membuat view 1 splash dengan menggunakan layout column dan menggunakan gambar

---
+++ app/src/main/java/com/example/p6meeting8/ui/view/screen/SplashView.kt
@@ -0,0 +1,38 @@
+package com.example.p6meeting8.ui.view.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.p6meeting8.R
+
+@Preview(showBackground = true)
+@Composable
+fun SplashView(
+    onMulaiButton: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.umylogo),
+            contentDescription = "",
+            modifier = Modifier.size(150.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+    }
+}
